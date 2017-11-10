3acb83ce440e561e45b572a2349a25a469b1cd58
Aayush Gupta <agupta234@sapient.com>
2017-11-10 16:21:38 +0530

    roche-pharma-cutomerportal : Updated Complete batch file

--- complete-build.bat
+++ complete-build.bat
@@ -6,10 +6,9 @@ cmd /C npm install grunt --save-dev
 cmd /C npm install -g bower
 cmd /C bower install
 cmd /C npm install aemsync -g
-cmd /C grunt aem
 cd ../../../../../../
 
 cmd /C  mvn enforcer:enforce
 cd roche.pharma.customerportal.complete
-cmd /C mvn clean install
+cmd /C mvn clean install -Dmaven.test.skip=true -PautoInstallPackage
 cd ../
