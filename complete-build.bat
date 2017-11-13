4d8e35df21582eb3d097fc5f954ee61c701be3ac
Varun Rawat <vrawat4@sapient.com>
2017-11-13 12:46:56 +0530

    grunt job added in batch file

--- complete-build.bat
+++ complete-build.bat
@@ -6,6 +6,7 @@ cmd /C npm install grunt --save-dev
 cmd /C npm install -g bower
 cmd /C bower install
 cmd /C npm install aemsync -g
+cmd /C grunt aem
 cd ../../../../../../
 
 cmd /C  mvn enforcer:enforce
