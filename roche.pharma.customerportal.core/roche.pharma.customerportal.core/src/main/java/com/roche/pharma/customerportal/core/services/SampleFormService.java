d928d14a2491486041642eb4b78e9d74f44d83ac
sagr25 <Vinayaka7@>
2017-11-20 17:52:09 +0530

    RPCP-52: Formatted code committed

--- roche.pharma.customerportal.core/roche.pharma.customerportal.core/src/main/java/com/roche/pharma/customerportal/core/services/SampleFormService.java
+++ roche.pharma.customerportal.core/roche.pharma.customerportal.core/src/main/java/com/roche/pharma/customerportal/core/services/SampleFormService.java
@@ -1,12 +1,11 @@
 package com.roche.pharma.customerportal.core.services;
 
-
-public interface  SampleFormService {
+public interface SampleFormService {
     
     /**
      * gets the input data of user URL
      * @return the input data of user
      */
-	public String injestFormData(String first, String last, String city); 
+    public String injestFormData(String first, String last, String city);
     
-  }
+}
