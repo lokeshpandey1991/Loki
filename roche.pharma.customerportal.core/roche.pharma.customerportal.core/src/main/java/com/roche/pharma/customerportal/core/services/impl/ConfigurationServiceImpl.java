f0ec20c6a340ff5020052cc62d8a6f29b17f6ae2
Vijay Kumar <vkumar116@sapient.com>
2017-11-27 23:18:39 +0530

    RPCP-44:AEM-Dynamic Media setUp and Impact analysis/technical Design for Image|review comments

--- roche.pharma.customerportal.core/roche.pharma.customerportal.core/src/main/java/com/roche/pharma/customerportal/core/services/impl/ConfigurationServiceImpl.java
+++ roche.pharma.customerportal.core/roche.pharma.customerportal.core/src/main/java/com/roche/pharma/customerportal/core/services/impl/ConfigurationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.roche.pharma.customerportal.core.services.impl;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -25,48 +26,63 @@ import com.roche.pharma.customerportal.core.services.ConfigurationService;
 @Service(value = ConfigurationService.class)
 @Component(immediate = true, metatype = true, label = "Global Configuration Service")
 @Properties({
-    @Property(name = "service.rootPath", value = "/content/customerportal",
-            description = "Root path for Roche website"),
-            @Property(name = "service.description", value = "Service for Dynamic Media Configuration"),
-            @Property(name = "image.service.url", label = "Dynamic Media Image Service Url",
-            description = "Set the Url for Dynamic Image Service", value = " "),
-            @Property(name = "video.service.url", label = "Dynamic Media Video Service Url",
-            description = "Set the Url for Dynamic Video Service", value = " "),
-            @Property(
-                    name = "media.service.configuration",
-                    label = "Dynamic Media Configuration",
-                    value = {
-                            "[ ]"
-                    },
-                    description = "Use syntax like herobanner-desktop-view=600,400,0.2,0.6,0.8,0.9 ({componentname-devicetype-view=width,height,crop-corordinates})",
-                    unbounded = PropertyUnbounded.ARRAY, cardinality = 20),
+        @Property(name = "service.rootPath", value = "/content/customerportal",
+                description = "Root path for Roche website"),
+        @Property(name = "productlogo.service.pagePath", value = "/content/customerportal/author-product-logos",
+                description = "Page path for Product Logo"),
+        @Property(name = "service.description", value = "Service for Global Configuration"),
+        @Property(name = "image.service.url", label = "Dynamic Media Image Service Url",
+                description = "Set the Url for Dynamic Image Service",
+                value = "https://roche-staging-h.assetsadobe2.com/is/image"),
+        @Property(name = "video.service.url", label = "Dynamic Media Video Service Url",
+                description = "Set the Url for Dynamic Video Service",
+                value = "https://gateway-eu.assetsadobe.com/DMGateway/"),
+        @Property(
+                name = "media.service.configuration",
+                label = "Dynamic Media Configuration",
+                value = {
+                    "herobanner-desktop-view=600,400,0.2,0.6,0.8,0.9"
+                },
+                description = "Use syntax like herobanner-desktop-view=600,400,0.2,0.6,0.8,0.9 ({componentname-devicetype-view=width,height,crop-corordinates})",
+                unbounded = PropertyUnbounded.ARRAY, cardinality = 20),
+        @Property(name = "activate.pdp.on.dtl.event", label = "Activate PDP On DTL Event",
+                description = "Activate PDP On DTL Event", boolValue = false),
+        @Property(name = "assay.menu.pages", label = "Assay Menu Pages Paths", value = {
+                "/content/customerportal/global/en/instrument", "/content/customerportal/us/en/instrument"
+        }, description = "Assay Menu Pages Paths", unbounded = PropertyUnbounded.ARRAY, cardinality = 50),
+        @Property(name = "dispatcher.flush.url", label = "Dispatcher Flush URL", value = {
+            "http://localhost/dispatcher/invalidate.cache"
+        }, unbounded = PropertyUnbounded.ARRAY, cardinality = 20),
 })
 public class ConfigurationServiceImpl implements ConfigurationService {
-
+    
     /** The Constant LOG. */
     private static final Logger LOG = LoggerFactory.getLogger(ConfigurationServiceImpl.class);
-
+    
+    /** The Constant CLASS_NAME. */
+    private static final String CLASS_NAME = "com.roche.pharma.customerportal.core.services.impl.ConfigurationServiceImpl";
+    
     /** The image service url. */
     private String imageServiceUrl;
-
+    
     /** The product logo path. */
     private String productLogoPath;
-
+    
     /** The video service url. */
     private String videoServiceUrl;
-
+    
     private List<String> dispatcherFlushURL;
-
+    
     /** The dynamic media conf map. */
     private Map<String, String> dynamicMediaConfMap;
-
+    
     private List<String> instrumentPages;
-
+    
     /** The root path of Roche website *. */
     private String rootPath;
-
+    
     Boolean activatePDPOnDTLUpdate;
-
+    
     /**
      * Activate.
      * @param properties the properties
@@ -75,7 +91,7 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public void activate(final Map<String, Object> properties) {
         readProperties(properties);
     }
-
+    
     /**
      * Read properties.
      * @param properties the properties this method set the properties
@@ -86,9 +102,14 @@ public class ConfigurationServiceImpl implements ConfigurationService {
         this.setVideoServiceUrl(properties.get("video.service.url").toString());
         this.setDynamicMediaConfMap(PropertiesUtil.toMap(properties.get("media.service.configuration"), new String[0]));
         this.setRootPath(properties.get("service.rootPath").toString());
-        
+        this.setProductLogoPath(properties.get("productlogo.service.pagePath").toString());
+        final String[] asseyMenuPaths = PropertiesUtil.toStringArray(properties.get("assay.menu.pages"));
+        final String[] disFlushURLs = PropertiesUtil.toStringArray(properties.get("dispatcher.flush.url"));
+        this.setInstrumentPages(Arrays.asList(asseyMenuPaths));
+        this.setDispatcherFlushURL(Arrays.asList(disFlushURLs));
+        this.setActivatePDPOnDTLUpdate(PropertiesUtil.toBoolean(properties.get("activate.pdp.on.dtl.event"), false));
     }
-
+    
     /**
      * Sets the Image service url. return Image Service URL
      * @return the image service url
@@ -97,7 +118,7 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public String getImageServiceUrl() {
         return imageServiceUrl;
     }
-
+    
     /**
      * Sets the image service url.
      * @param imageServiceUrl the new image service url
@@ -105,7 +126,7 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public void setImageServiceUrl(final String imageServiceUrl) {
         this.imageServiceUrl = imageServiceUrl;
     }
-
+    
     /**
      * Sets the video service url. return Video Service URL
      * @return the video service url
@@ -114,7 +135,7 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public String getVideoServiceUrl() {
         return videoServiceUrl;
     }
-
+    
     /**
      * Sets the video service url.
      * @param videoServiceUrl the new video service url
@@ -122,7 +143,7 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public void setVideoServiceUrl(final String videoServiceUrl) {
         this.videoServiceUrl = videoServiceUrl;
     }
-
+    
     /**
      * Gets the dynamic media conf map.
      * @return the dynamic media conf map
@@ -131,7 +152,7 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public Map<String, String> getDynamicMediaConfMap() {
         return dynamicMediaConfMap;
     }
-
+    
     /**
      * Sets the dynamic media conf map.
      * @param dynamicMediaConfMap the dynamic media conf map
@@ -139,7 +160,7 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public void setDynamicMediaConfMap(final Map<String, String> dynamicMediaConfMap) {
         this.dynamicMediaConfMap = dynamicMediaConfMap;
     }
-
+    
     /*
      * (non-Javadoc)
      * @see com.roche.pharma.customerportal.core.services.ConfigurationService#getRootPath()
@@ -148,7 +169,7 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public String getRootPath() {
         return rootPath;
     }
-
+    
     /**
      * Sets the root path.
      * @param rootPath the new root path
@@ -156,7 +177,7 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public void setRootPath(final String rootPath) {
         this.rootPath = rootPath;
     }
-
+    
     /**
      * Gets the product logo path.
      * @return the product logo path
@@ -165,7 +186,7 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public String getProductLogoPath() {
         return productLogoPath;
     }
-
+    
     /**
      * Sets the product logo path.
      * @param productLogoPath the new product logo path
@@ -173,7 +194,7 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public void setProductLogoPath(final String productLogoPath) {
         this.productLogoPath = productLogoPath;
     }
-
+    
     /*
      * (non-Javadoc)
      * @see com.roche.pharma.customerportal.core.services.ConfigurationService#getInstrumentPages()
@@ -182,14 +203,14 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public List<String> getInstrumentPages() {
         return instrumentPages;
     }
-
+    
     /**
      * @param instrumentPages
      */
     public void setInstrumentPages(final List<String> instrumentPages) {
         this.instrumentPages = instrumentPages;
     }
-
+    
     /**
      * @return
      */
@@ -197,14 +218,14 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public List<String> getDispatcherFlushURL() {
         return dispatcherFlushURL;
     }
-
+    
     /**
      * @param dispatcherFlushURL
      */
     public void setDispatcherFlushURL(final List<String> dispatcherFlushURL) {
         this.dispatcherFlushURL = dispatcherFlushURL;
     }
-
+    
     /*
      * (non-Javadoc)
      * @see com.roche.pharma.customerportal.core.services.ConfigurationService#isActivatePDPOnDTLUpdate()
@@ -213,12 +234,12 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public Boolean isActivatePDPOnDTLUpdate() {
         return activatePDPOnDTLUpdate;
     }
-
+    
     /**
      * @param activatePDPOnDTLUpdate
      */
     public void setActivatePDPOnDTLUpdate(final Boolean activatePDPOnDTLUpdate) {
         this.activatePDPOnDTLUpdate = activatePDPOnDTLUpdate;
     }
-
+    
 }
